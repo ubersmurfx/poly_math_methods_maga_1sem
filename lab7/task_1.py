ade92e3506b9c72a21c97e3e46e54b53e6be3094
Georgiy Kazantsev <113335397+ubersmurfx@users.noreply.github.com>
2024-12-08 17:24:53 +0300

    Update lab7.py

--- lab7/task_1.py
+++ lab7/task_1.py
@@ -1,83 +1,98 @@
-import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import random
+import math
+import keyboard
+from time import sleep
+import threading
 
-# https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
-# https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
-def draw_triangles(*triangles: np.array):
-    for tr in triangles:
-        tr = np.append(tr, [tr[0]], axis=0)
-        x, y = tr.T
-        plt.scatter(x, y)
-        plt.plot(x, y)
+class Triangle:
+    def __init__(self, a=Point(), b=Point(), c=Point()):
+        self.ds = [a, b, c]
 
-    plt.grid()
-    plt.show()
-
-
-def ccw(a, b, c):
-    return (c[1]-a[1]) * (b[0]-a[0]) > (b[1]-a[1]) * (c[0]-a[0])
-
-
-# Return true if line segments AB and CD intersect
-def intersect(a, b, c, d):
-    return ccw(a, c, d) != ccw(b, c, d) and ccw(a, b, c) != ccw(a, b, d)
+    def plot(self, color='b'):
+        x = [p.x for p in self.ds] + [self.ds[0].x]
+        y = [p.y for p in self.ds] + [self.ds[0].y]
+        plt.plot(x, y, color + '-')
+        plt.fill(x,y, color='gray', alpha=0.2)
 
 
-def same_side(p, a, b, c):
-    cp1 = np.cross(c-b, p-b)
-    cp2 = np.cross(c-b, a-b)
-    # print(cp1, cp2, np.dot(cp1, cp2))
-    if np.dot(cp1, cp2) >= 0:
-        # print(f'{p}, {a}, {b}, {c} {True}')
-        return True
-    else:
-        # print(f'{p}, {a}, {b}, {c} {False}')
+def is_point_to_triangle(p, t):
+    for i in range(3):
+        j = (i + 1) % 3
+        k = (p.x - t.ds[i].x) * (t.ds[i].y - t.ds[j].y) - (p.y - t.ds[i].y) * (t.ds[i].x - t.ds[j].x)
+        if k < 0:
+            return False
+    return True
+
+def is_intersect_line(a1, a2, b1, b2):
+    b = Point(a2.x - a1.x, a2.y - a1.y)
+    d = Point(b2.x - b1.x, b2.y - b1.y)
+
+    dot = b.x * d.y - b.y * d.x
+    if dot == 0:
         return False
 
-
-def point_in_triangle(p, tr):
-    a = tr[0]
-    b = tr[1]
-    c = tr[2]
-
-    if same_side(p, a, b, c) and same_side(p, b, a, c) and same_side(p, c, a, b):
-        return True
-    else:
+    c = Point(b1.x - a1.x, b1.y - a1.y)
+    t = (c.x * d.y - c.y * d.x) / dot
+    if t < 0.0 or t > 1.0:
         return False
 
+    t = (c.x * b.y - c.y * b.x) / dot
+    return not (t < 0.0 or t > 1.0)
 
-# Оказалось, вариант с точками не работает, когда этих самых внутренних точек нет
-def check_collision(*triangles):
-    tr_1 = triangles[0]
-    tr_2 = triangles[1]
-
-    for point in tr_1:
-        if point_in_triangle(point, tr_2):
+def is_intersect_triangle(a, b):
+    for p in range(3):
+        if is_point_to_triangle(a.ds[p], b) or is_point_to_triangle(b.ds[p], a):
             return True
 
-    for point in tr_2:
-        if point_in_triangle(point, tr_1):
-            return True
-
-    for i in reversed(range(3)):
-        for j in reversed(range(3)):
-            if intersect(tr_1[i], tr_1[i-1], tr_2[j], tr_2[j-1]):
+    for i in range(3):
+        for j in range(3):
+            if is_intersect_line(a.ds[i], a.ds[(i + 1) % 3], b.ds[j], b.ds[(j + 1) % 3]):
                 return True
-
     return False
 
 
-tr_1 = np.array([[1, 1],
-                [2, 3],
-                [4, 1]])
+def main():
+    a = Triangle(
+            Point(round(random.uniform(0, 300), 1), 50), 
+            Point(150, round(random.uniform(0, 300), 1)), 
+            Point(round(random.uniform(0, 300), 1), 100)
+        )
+    b= Triangle(
+            Point(round(random.uniform(0, 300), 1), 50), 
+            Point(150, round(random.uniform(0, 300), 1)), 
+            Point(round(random.uniform(0, 300), 1), 100)
+        )
+    
+    if is_intersect_triangle(a, b):
+        print("Yes.")
+    else:
+        print("No!")
+    a.plot('r')
+    b.plot('g')
 
-# border collision
-tr_2 = np.array([[5, 1],
-                [3, 3],
-                [4, 3]])
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title(f"Triangles intersect is {is_intersect_triangle(a, b)}")
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.xlim(0, 300)
+    plt.ylim(0, 300)
+    plt.grid(True)
+    plt.show(block=False)
+    plt.pause(1)
+    plt.close('all')
+    plt.clf()
 
-# print(intersect(tr_1[1], tr_1[2], tr_2[0], tr_2[1]))
+if __name__=="__main__":
+    i = 0
+    while i < 10:
+        i =+ 1
+        main()
+        continue
 
-print(check_collision(tr_1, tr_2))
-draw_triangles(tr_1, tr_2)
+    
